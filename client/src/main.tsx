0acc704ed8c9ee152c6f897d4f565b1aa423b36c
이원영 <leewonyoung@iwon-yeong-ui-Macmini.local>
2026-03-23 10:25:29 +0900

    main.tsx seedData() 호출 제거 - 더미 데이터 자동 초기화 완전 차단

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { seedData } from "./lib/seed";
-
-// ?reseed=1 파라미터로 강제 재시드 가능
-if (new URLSearchParams(window.location.search).get('reseed') === '1') {
-  const keys = ['ames_items','ames_vendors','ames_samples','ames_orders','ames_boms','ames_settlements','ames_sales','ames_purchases','ames_expenses','ames_seed_v1'];
-  keys.forEach(k => localStorage.removeItem(k));
-  window.history.replaceState({}, '', window.location.pathname);
-}
-
-seedData();
 
 createRoot(document.getElementById("root")!).render(<App />);
